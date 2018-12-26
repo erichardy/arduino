37b452b29a0387a7c3364d70cde6ef941606df64
Eric Hardy <Eric.Hardy@univ-brest.fr>
2018-12-26 01:53:06 +0100

    avec 74HC595

--- railroad_switch/railSwitch/railSwitch.ino
+++ railroad_switch/railSwitch/railSwitch.ino
@@ -1,10 +1,37 @@
 
 #include <Servo.h>
+#define ALL 255
+#define NO_ONE 0
 
 int angle0 = 5;
 int angle = 53;
-int buttonPin = 10;
+int buttonPin = 7;
 int button_delay = 300;
+
+/* commandes des leds par 74HC595 */
+int latchPin = 11;
+int clockPin = 9;
+int dataPin = 12;
+byte leds = 0;
+/* Numeros des leds */
+int led1_1 = 128;
+int led1_2 = 64;
+int led2_1 = 32;
+int led2_2 = 16;
+int led3_1 = 8;
+int led3_2 = 4;
+int led4_1 = 2;
+int led4_2 = 1;
+
+/* commandes des servos */
+int servo1 = 8;
+
+void turnOnLight(int light)
+{
+   digitalWrite(latchPin, LOW);
+   shiftOut(dataPin, clockPin, LSBFIRST, light);
+   digitalWrite(latchPin, HIGH);
+}
 
 class railSwitch: public Servo {
   public:
@@ -25,11 +52,13 @@ class railSwitch: public Servo {
       if (current_state == 0) {
         this->write(angle);
         this->state = 1;
+        turnOnLight(led1_2);
         Serial.print("state = 1\n");
       }
       if (current_state == 1) {
         this->init_state();
         this->state = 0;
+        turnOnLight(led1_1);
         Serial.print("state = 0\n");
       }
     }
@@ -39,14 +68,23 @@ railSwitch aiguil1 ;
 
 void setup() {
   pinMode(buttonPin, INPUT_PULLUP);
-  aiguil1.attach(9);
+  aiguil1.attach(servo1);
   aiguil1.init_state();
   Serial.begin(9600);
+
+  /* initialisation du 74HC595 */
+  pinMode(latchPin, OUTPUT);
+  pinMode(dataPin, OUTPUT);
+  pinMode(clockPin, OUTPUT);
+
+  turnOnLight(NO_ONE);
 }
 
 void loop() {
   if (digitalRead(buttonPin) == LOW) {
     aiguil1.change_state();
+    Serial.print("buttonPin : ");
+    Serial.println(digitalRead(buttonPin), 1);
     delay(button_delay);
   }
   /*
