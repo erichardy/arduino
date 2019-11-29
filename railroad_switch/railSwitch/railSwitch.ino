e820303482b67fd1a83696a0ce55c4e60fa1520d
Eric Hardy <Eric.Hardy@univ-brest.fr>
2019-11-29 20:29:50 +0100

    etallonnage

--- railroad_switch/railSwitch/railSwitch.ino
+++ railroad_switch/railSwitch/railSwitch.ino
@@ -12,6 +12,7 @@ class railSwitch: public Servo {
     int current_state;
     unsigned char angle0;
     unsigned char angle;
+    unsigned char alpha; // angle pour tests
     unsigned char command;
     unsigned char button;
 
@@ -19,6 +20,7 @@ class railSwitch: public Servo {
       this->write(this->angle0);
       this->state = 0;
     }
+
     int other_state() {
       this->write(this->angle);
       this->state = 1;
@@ -46,8 +48,8 @@ railSwitch aiguil1;
 railSwitch aiguils[NBSWITCH];
 
 /* les angles (initial et autre) de chaque servo */
-unsigned char angles0[NBSWITCH] = {5, 5, 5, 5, 5};
-unsigned char angles[NBSWITCH] = {130, 53, 53, 53, 180};
+unsigned char angles0[NBSWITCH] = {30, 25, 30, 30, 20};
+unsigned char angles[NBSWITCH] = {120, 120, 135, 125, 120};
 
 /* les pins des commandes des servo */
 unsigned char commands[NBSWITCH] = {9, 10, 11, 12, 13};
@@ -55,18 +57,48 @@ unsigned char buttons[NBSWITCH] = {8, 7, 6, 5, 4};
 
 unsigned char i;
 
+/*
+ * Micro Servo 9g :
+ * marron : masse
+ * rouge : +5V (milieu)
+ * jaune : commande
+ */
 void setup() {
+  delay(1000);
   Serial.begin(9600);
   for (i = 0; i < NBSWITCH; i++){
-    Serial.print(i);
-    Serial.print("\n");
+    Serial.print("Aiguillage : ");
+    Serial.println(i + 1);
+    Serial.print("button : ");
+    Serial.println(i);
     pinMode(buttons[i], INPUT_PULLUP);
     aiguils[i].angle0 = angles0[i];
     aiguils[i].angle = angles[i];
+    aiguils[i].alpha = 0;
     aiguils[i].command = commands[i];
+    
     aiguils[i].button = buttons[i];
+    Serial.print("Etat : ");
+    Serial.println(aiguils[i].state);
     aiguils[i].attach(aiguils[i].command);
-    aiguils[i].init_state();
+    Serial.print("Attached ? ");
+    Serial.println(aiguils[i].attached());
+    if (aiguils[i].attached()) {
+        aiguils[i].init_state();
+        delay(500);
+        aiguils[i].change_state();
+        Serial.print("Etat : ");
+        Serial.println(aiguils[i].state);
+        delay(500);
+        aiguils[i].change_state();
+        Serial.print("Etat : ");
+        Serial.println(aiguils[i].state);
+        delay(500);
+        aiguils[i].init_state();
+        Serial.print("Etat : ");
+        Serial.println(aiguils[i].state);
+        Serial.println("------------------------");
+    }
   }
   i = 0;
 }
@@ -112,9 +144,12 @@ void loop() {
     i = 0;
   }
   if (digitalRead(int(aiguils[i].button)) == LOW){
+    Serial.print("Aiguillage : ");
+    Serial.print(i + 1);
+    Serial.print(" , ");
+    // Serial.print(" boutton : ");
+    // Serial.println(aiguils[i].button, DEC);
     aiguils[i].change_state();
-    Serial.print("boutton : ");
-    Serial.println(aiguils[i].button, DEC);
     delay(button_delay);
   }
   i++;
