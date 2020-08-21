63a34fd10601eab79eb9206251aa5310248aca55
Eric Hardy <Eric.Hardy@univ-brest.fr>
2020-08-21 21:55:45 +0200

    sonde temp + alarme

--- NANO/DS18B20/temperatureAlarm/temperatureAlarm.ino
+++ NANO/DS18B20/temperatureAlarm/temperatureAlarm.ino
@@ -3,9 +3,16 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
 
+#define TONE 8
 #define POT_PIN A2
 #define DS18B20_PIN 9
 #define TEMPERATURE_PRECISION 12
+#define BASE_FREQ 440
+int freq = 440;
+bool sirenUp = 0;
+
+#define nbFreqs 10
+int freqs[nbFreqs] = {440, 550, 660, 770, 880, 990, 1000, 1220, 1440, 1550};
 
 
 LiquidCrystal lcd(12, 11, 7, 6, 5, 4);
@@ -26,7 +33,6 @@ float tempC; // Current temperature
 float tempT = 25.0; // Target temperature
 int setTarget, target;
 bool alarm = 0; // alarm set or not set
-int volume = 0; // Buzzer volume
 
 // function to print a device address
 void printAddress(DeviceAddress deviceAddress)
@@ -41,32 +47,32 @@ void printAddress(DeviceAddress deviceAddress)
 // dispTemps : display temperatures, current and target
 int lcdTemps(float current, float target) {
   lcd.setCursor(0 ,0);
-  lcd.print("               ");
-  lcd.setCursor(0, 0);
+  lcd.print("Actuelle :       ");
+           // 01234567890123456
+  lcd.setCursor(11, 0);
   lcd.print(current);
-  lcd.print("->");
+  lcd.setCursor(0, 1);
+  lcd.print("Alarme a :       ");
+          // 01234567890123456
+  lcd.setCursor(11, 1);
   lcd.print(target);
 }
 
-int lcdAlarm(bool alarm){
-  lcd.setCursor(8, 1);
-  lcd.print(" ");
-  lcd.setCursor(0, 1);
-  lcd.print("Alarm ");
-  if (alarm) {
-    lcd.print("ON");
+void siren(int offset) {
+  int f;
+  if (sirenUp) {
+    for (f = 0; f < nbFreqs; f++) {
+      tone(TONE, freqs[f]);
+      delay(50);
+    }
+    noTone(TONE);
+  } else {
+    for (f = nbFreqs - 1; f >= 0; f--) {
+      tone(TONE, freqs[f]);
+      delay(50);
+    }
+    noTone(TONE);
   }
-  else {
-    lcd.print("OFF");
-  }
-}
-
-int lcdVolume(int volume){
-  lcd.setCursor(13, 1);
-  lcd.print("   ");
-  lcd.setCursor(10, 1);
-  lcd.print("Vol ");
-  lcd.print(volume);
 }
 
 void setup() {
@@ -89,6 +95,8 @@ void setup() {
   printAddress(insideThermometer);
   Serial.println();
   sensors.setResolution(insideThermometer, TEMPERATURE_PRECISION);
+
+  noTone(TONE);
 }
 
 void loop() {
@@ -98,17 +106,26 @@ void loop() {
   Serial.println(tempC);
   setTarget = analogRead(POT_PIN);
   target = map(setTarget, 0, 1024, 0, 1400);
-  Serial.println(target);
-  Serial.println(target / 10.0);
   tempT = target / 10.0;
+  Serial.println(tempT);
   lcdTemps(tempC, tempT); // pour tests + 76.0
-  lcdAlarm(alarm);
+  if (tempC >= tempT) {
+    if (tempT > 1) {
+      siren(0);
+    } else {
+      noTone(TONE);
+    }
+  } else {
+    noTone(TONE);
+  }
+  sirenUp = not(sirenUp);
+  /*
   lcdVolume(volume);
   volume += 5;
   if (volume > 99) {
     volume = 1;
   }
   alarm = not(alarm);
-  
-  delay(200);
+  */
+  // delay(200);
 }
